405bbd24d36283c90fd7ff1418cf262804f9c008
Saminda Abeyruwan <you@example.com>
2019-07-16 17:37:11 -0700

    updates

--- torch-cpp/main.cpp
+++ torch-cpp/main.cpp
@@ -13,11 +13,19 @@
 #include "absl/flags/parse.h"
 #include "absl/strings/substitute.h"
 
-ABSL_FLAG(std::string, data_root, "", "Data root");
-ABSL_FLAG(int64_t, train_batch_size, 64, "Train batch size");
+ABSL_FLAG(std::string, data_root, "", "Where to find the MNIST dataset");
+ABSL_FLAG(int64_t, train_batch_size, 64, "The batch size for training");
 ABSL_FLAG(int64_t, test_batch_size, 1000, "Test batch size");
-ABSL_FLAG(int64_t, number_of_epochs, 5, "Number of epochs");
+ABSL_FLAG(int64_t, number_of_epochs, 30, "The number of epochs to train");
+ABSL_FLAG(int64_t, batch_size, 64, "The batch size for training");
 ABSL_FLAG(int64_t, log_interval, 10, "Log interval");
+ABSL_FLAG(int64_t, noise_size, 100, "The size of the noise vector fed to the generator");
+ABSL_FLAG(int64_t, checkpoint_every, 200, "After how many batches to create a new checkpoint periodically");
+ABSL_FLAG(int64_t, number_of_samples_per_checkpoint, 10, "How many images to sample at every checkpoint");
+ABSL_FLAG(bool,
+          restore_from_checkpoint,
+          false,
+          "Set to 'true' to restore models and optimizers from previously saved checkpoints");
 
 void testBasicTensorOps() {
   auto mat = torch::rand({3, 3});
@@ -321,12 +329,166 @@ void testPoly() {
   std::cout << absl::Substitute("==> Actual function:\t $0", poly_desc(w_target.view({-1}), b_target)) << std::endl;
 }
 
+void testDcgan() {
+  torch::manual_seed(1);
+
+  // Create the device we pass around based on whether CUDA is available.
+  torch::Device device(torch::kCPU);
+  if (torch::cuda::is_available()) {
+    std::cout << "CUDA is available! Training on GPU" << std::endl;
+    device = torch::Device(torch::kCUDA);
+  }
+
+  torch::nn::Sequential generator(
+      // Layer 1
+      torch::nn::Conv2d(torch::nn::Conv2dOptions(absl::GetFlag(FLAGS_noise_size), 256, 4)
+                            .with_bias(false)
+                            .transposed(true)),
+      torch::nn::BatchNorm(256),
+      torch::nn::Functional(torch::relu),
+      // Layer 2
+      torch::nn::Conv2d(torch::nn::Conv2dOptions(256, 128, 3)
+                            .stride(2)
+                            .padding(1)
+                            .with_bias(false)
+                            .transposed(true)),
+      torch::nn::BatchNorm(128),
+      torch::nn::Functional(torch::relu),
+      // Layer 3
+      torch::nn::Conv2d(torch::nn::Conv2dOptions(128, 64, 4)
+                            .stride(2)
+                            .padding(1)
+                            .with_bias(false)
+                            .transposed(true)),
+      torch::nn::BatchNorm(64),
+      torch::nn::Functional(torch::relu),
+      // Layer 4
+      torch::nn::Conv2d(torch::nn::Conv2dOptions(64, 1, 4)
+                            .stride(2)
+                            .padding(1)
+                            .with_bias(false)
+                            .transposed(true)),
+      torch::nn::Functional(torch::tanh)
+  );
+  generator->to(device);
+
+  torch::nn::Sequential discriminator(
+      // Layer 1
+      torch::nn::Conv2d(torch::nn::Conv2dOptions(1, 64, 4).stride(2).padding(1).with_bias(false)),
+      torch::nn::Functional(torch::leaky_relu, 0.2),
+      // Layer 2
+      torch::nn::Conv2d(torch::nn::Conv2dOptions(64, 128, 4).stride(2).padding(1).with_bias(false)),
+      torch::nn::BatchNorm(128),
+      torch::nn::Functional(torch::leaky_relu, 0.2),
+      // Layer 3
+      torch::nn::Conv2d(torch::nn::Conv2dOptions(128, 256, 4).stride(2).padding(1).with_bias(false)),
+      torch::nn::BatchNorm(256),
+      torch::nn::Functional(torch::leaky_relu, 0.2),
+      // Layer 4
+      torch::nn::Conv2d(torch::nn::Conv2dOptions(256, 1, 3).stride(1).padding(0).with_bias(false)),
+      torch::nn::Functional(torch::sigmoid));
+  discriminator->to(device);
+
+
+  // Assume the MNIST dataset is available under `kDataFolder`;
+  auto dataset = torch::data::datasets::MNIST(absl::GetFlag(FLAGS_data_root))
+      .map(torch::data::transforms::Normalize<>(0.5, 0.5))
+      .map(torch::data::transforms::Stack<>());
+  const int64_t batches_per_epoch =
+      std::ceil(dataset.size().value() / static_cast<double>(absl::GetFlag(FLAGS_batch_size)));
+
+  auto data_loader = torch::data::make_data_loader(
+      std::move(dataset),
+      torch::data::DataLoaderOptions().batch_size(absl::GetFlag(FLAGS_batch_size)).workers(2));
+
+  torch::optim::Adam generator_optimizer(
+      generator->parameters(), torch::optim::AdamOptions(2e-4).beta1(0.5));
+  torch::optim::Adam discriminator_optimizer(
+      discriminator->parameters(), torch::optim::AdamOptions(2e-4).beta1(0.5));
+
+  if (absl::GetFlag(FLAGS_restore_from_checkpoint)) {
+    torch::load(generator, "generator-checkpoint.pt");
+    torch::load(generator_optimizer, "generator-optimizer-checkpoint.pt");
+    torch::load(discriminator, "discriminator-checkpoint.pt");
+    torch::load(discriminator_optimizer, "discriminator-optimizer-checkpoint.pt");
+  }
+
+  int64_t checkpoint_counter = 1;
+  for (int64_t epoch = 1; epoch <= absl::GetFlag(FLAGS_number_of_epochs); ++epoch) {
+    int64_t batch_index = 0;
+    for (torch::data::Example<> &batch : *data_loader) {
+      // Train discriminator with real images.
+      discriminator->zero_grad();
+      torch::Tensor real_images = batch.data.to(device);
+      torch::Tensor real_labels =
+          torch::empty(batch.data.size(0), device).uniform_(0.8, 1.0);
+      torch::Tensor real_output = discriminator->forward(real_images);
+      torch::Tensor d_loss_real =
+          torch::binary_cross_entropy(real_output, real_labels);
+      d_loss_real.backward();
+
+      // Train discriminator with fake images.
+      torch::Tensor noise =
+          torch::randn({batch.data.size(0), absl::GetFlag(FLAGS_noise_size), 1, 1}, device);
+      torch::Tensor fake_images = generator->forward(noise);
+      torch::Tensor fake_labels = torch::zeros(batch.data.size(0), device);
+      torch::Tensor fake_output = discriminator->forward(fake_images.detach());
+      torch::Tensor d_loss_fake =
+          torch::binary_cross_entropy(fake_output, fake_labels);
+      d_loss_fake.backward();
+
+      torch::Tensor d_loss = d_loss_real + d_loss_fake;
+      discriminator_optimizer.step();
+
+      // Train generator.
+      generator->zero_grad();
+      fake_labels.fill_(1);
+      fake_output = discriminator->forward(fake_images);
+      torch::Tensor g_loss =
+          torch::binary_cross_entropy(fake_output, fake_labels);
+      g_loss.backward();
+      generator_optimizer.step();
+      batch_index++;
+      if (batch_index % absl::GetFlag(FLAGS_log_interval) == 0) {
+        std::printf(
+            "\r[%2ld/%2ld][%3ld/%3ld] D_loss: %.4f | G_loss: %.4f\n",
+            epoch,
+            absl::GetFlag(FLAGS_number_of_epochs),
+            batch_index,
+            batches_per_epoch,
+            d_loss.item<float>(),
+            g_loss.item<float>());
+      }
+
+      if (batch_index % absl::GetFlag(FLAGS_checkpoint_every) == 0) {
+        // Checkpoint the model and optimizer state.
+        torch::save(generator, "generator-checkpoint.pt");
+        torch::save(generator_optimizer, "generator-optimizer-checkpoint.pt");
+        torch::save(discriminator, "discriminator-checkpoint.pt");
+        torch::save(
+            discriminator_optimizer, "discriminator-optimizer-checkpoint.pt");
+        // Sample the generator and save the images.
+        torch::Tensor samples = generator->forward(torch::randn(
+            {absl::GetFlag(FLAGS_number_of_samples_per_checkpoint), absl::GetFlag(FLAGS_noise_size), 1, 1}, device));
+        torch::save(
+            (samples + 1.0) / 2.0,
+            torch::str("dcgan-sample-", checkpoint_counter, ".pt"));
+        std::cout << "\n-> checkpoint " << ++checkpoint_counter << '\n';
+      }
+    }
+  }
+
+  std::cout << "Training complete!" << std::endl;
+
+}
+
 int main(int argc, char **argv) {
   absl::ParseCommandLine(argc, argv);
   std::cout << "*** Torch ST ***" << std::endl;
 //  testModel();
 //  testTrainTestMnist();
-  testPoly();
+//  testPoly();
+  testDcgan();
   std::cout << "*** Torch EN ***" << std::endl;
   return 0;
 }
